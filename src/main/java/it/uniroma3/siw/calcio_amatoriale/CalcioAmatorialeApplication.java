1a414d147fdd9b44acc150e38c4f439296013e04
mattiaambrosio <mat.ambrosio@stud.uniroma3.it>
2026-04-24 14:56:08 +0200

    Primo commit: progetto generato con Spring Initializr

---
+++ src/main/java/it/uniroma3/siw/calcio_amatoriale/CalcioAmatorialeApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.calcio_amatoriale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcioAmatorialeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcioAmatorialeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/calcio_amatoriale/CalcioAmatorialeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.calcio_amatoriale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcioAmatorialeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
